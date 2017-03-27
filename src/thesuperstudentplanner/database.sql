8465ef1c40e8174c3319c597da1ee4e22925fe70
daniawareh <daniawareh@danias-mbp.westell.com>
2017-03-27 16:10:45 -0700

    updated database

--- src/thesuperstudentplanner/database.sql
+++ src/thesuperstudentplanner/database.sql
@@ -1,27 +1,50 @@
---TEST
 
-CREATE TABLE Appointments (
-SID char (9) NOT NULL,
-TITLE varchar (30) NOT NULL,
-"DAY" varchar (10) NOT NULL,
-STARTTIME varchar (4) NOT NULL, 
-ENDTIME varchar (4) NOT NULL,
-CONSTRAINT Appointments_pk PRIMARY KEY (SID, "DAY",STARTTIME)
-);
 
-CREATE TABLE Students (
-SID char (9) NOT NULL,
-FIRSTNAME varchar (10),
-LASTNAME varchar (10),
-CONSTRAINT Students_pk PRIMARY KEY (SID)
-);
+CREATE TABLE students
+  (
+  studentusername VARCHAR (20) NOT NULL,
+  firstName VARCHAR(20) NOT NULL,
+  lastName VARCHAR(20) NOT NULL,
+  CONSTRAINT pk_students PRIMARY KEY (studentusername)
+  );
 
-CREATE TABLE Professor (
-SID char (9) NOT NULL,
-FIRSTNAME varchar (10),
-LASTNAME varchar (10),
-CONSTRAINT Professor_pk PRIMARY KEY (SID)
-);
 
-SELECT * from Appointments;
+CREATE TABLE professors
+  (
+  profusername VARCHAR (20) NOT NULL,
+  firstName VARCHAR(20) NOT NULL,
+  lastName VARCHAR(20) NOT NULL,
+  CONSTRAINT pk_professors PRIMARY KEY (profusername)
+  );
+
+CREATE TABLE appointments
+ (
+  title VARCHAR(20) NOT NULL,
+  "DATE" DATE NOT NULL,
+  starttime TIME NOT NULL,
+  endtime TIME NOT NULL,
+  studentusername VARCHAR (20) NOT NULL REFERENCES students(studentusername),
+  CONSTRAINT pk_appointments PRIMARY KEY ("DATE",title,starttime, endtime),
+  CONSTRAINT fk1_appointments FOREIGN KEY (studentusername) REFERENCES students(studentusername)
+  );
+
+
+INSERT INTO students 
+    VALUES ('anncook','Ann','Cook'),
+    ('ralphvasquez', 'Ralph', 'Vasquez'),
+    ('willieallen', 'Willie', 'Allen'),
+    ('juliepeters','Julie','Peters'),
+    ('robertcohen', 'Robert', 'Cohen'),
+    ('ericaaustin', 'Erica','Austin'),
+    ('anglowong','Angelo', 'Wong'),
+    ('irvinmiller', 'Irvin', 'Miller'),
+    ('lindsayparsons', 'Lindsay', 'Parsons'),
+    ('jamiereed', 'Jamie', 'Reed');
+
+INSERT INTO professors
+    VALUES ('feihoffman', 'Fei', 'Hoffman');
+
+    
+INSERT INTO appointments
+    VALUES ('CECS 341', '2017-03-27', '9:00', '10:00','anncook');
 
